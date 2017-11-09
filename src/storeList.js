abedeecd9582e2b1ebf12ee8544247cb03701aa0
Adam Nguyen <adamngu@gmail.com>
2017-11-09 21:11:08 +0100

    hack solution to get flexbox elements centered: NEEDS REFACTORING EVENTUALLY

--- src/storeList.js
+++ src/storeList.js
@@ -4,6 +4,28 @@ import './css/storeList.css';
 import Loader from './loader';
 
 class StoreList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {width: 0};
+  }
+
+  componentWillMount = () => {
+    this.updateStoreListWidth();
+  }
+
+  componentDidMount = () => {
+    window.addEventListener("resize", this.updateStoreListWidth);
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener("resize", this.updateStoreListWidth);
+  }
+
+  updateStoreListWidth = () => {
+    const componentWidth = 340 * Math.floor((window.innerWidth)/340);
+    this.setState({width: componentWidth });
+  }
+
   renderStores = () => {
     const stores = this.props.stores;
     return stores.map((store, storeIndex) => {
@@ -19,11 +41,13 @@ class StoreList extends Component {
   }
 
   render(){
+    const width = this.state.width;
+    const marginLeft = (window.innerWidth - width)/2 - 10;
     if(this.props.stores.length === 0) {
       return <Loader/>;
     } else {
       return (
-        <div className="store-list">
+        <div className="store-list" style={{width: width, marginLeft: marginLeft}}>
           {this.renderStores()}
         </div>
       );
